7e2c06fc023867dad1800e6fc5fa041ff7033438
Ceveline <74314874+ceveline@users.noreply.github.com>
2023-11-20 13:00:29 -0500

    Imported the images from the old project and also the buttons were changed

--- Trevys-Iconic-Pizza--TrevBranch2/Trev_TrevysIconicPizza/Trev_TrevysIconicPizza/TrevysIconicPizza/PizzaPage.Designer.cs
+++ Trevys-Iconic-Pizza--TrevBranch2/Trev_TrevysIconicPizza/Trev_TrevysIconicPizza/TrevysIconicPizza/PizzaPage.Designer.cs
@@ -34,27 +34,31 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(73, 121);
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(90)))), ((int)(((byte)(37)))));
+            this.button1.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(73, 205);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(187, 67);
             this.button1.TabIndex = 0;
             this.button1.Text = "PIZZA";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(309, 121);
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(90)))), ((int)(((byte)(37)))));
+            this.button2.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(309, 205);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(187, 67);
             this.button2.TabIndex = 1;
             this.button2.Text = "DRINKS";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button2.UseVisualStyleBackColor = false;
             // 
             // MenuPage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Coral;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(239)))), ((int)(((byte)(180)))), ((int)(((byte)(100)))));
             this.ClientSize = new System.Drawing.Size(568, 309);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
