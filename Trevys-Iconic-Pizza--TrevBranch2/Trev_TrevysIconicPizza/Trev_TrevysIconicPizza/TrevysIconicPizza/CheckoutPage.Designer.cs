58d4c95f6e6b2ef6c2795d8a326feec7de09bc33
Baelong <fruhamsan@gmail.com>
2023-11-20 22:25:53 -0500

    test push

---
+++ Trevys-Iconic-Pizza--TrevBranch2/Trev_TrevysIconicPizza/Trev_TrevysIconicPizza/TrevysIconicPizza/CheckoutPage.Designer.cs
@@ -0,0 +1,307 @@
+﻿namespace TrevysIconicPizza
+{
+    partial class CheckoutPage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.paymentGroupBox = new System.Windows.Forms.GroupBox();
+            this.phoneTextBox = new System.Windows.Forms.TextBox();
+            this.phoneLabel = new System.Windows.Forms.Label();
+            this.serviceLabel = new System.Windows.Forms.Label();
+            this.addressTextBox = new System.Windows.Forms.TextBox();
+            this.pickupRadioButton = new System.Windows.Forms.RadioButton();
+            this.CVVTextBox = new System.Windows.Forms.TextBox();
+            this.deliveryRadioButton = new System.Windows.Forms.RadioButton();
+            this.CVVLabel = new System.Windows.Forms.Label();
+            this.addressLabel = new System.Windows.Forms.Label();
+            this.orderGroupBox = new System.Windows.Forms.GroupBox();
+            this.checkoutButton = new System.Windows.Forms.Button();
+            this.totalLabel = new System.Windows.Forms.Label();
+            this.totalTextBox = new System.Windows.Forms.TextBox();
+            this.orderListBox = new System.Windows.Forms.ListBox();
+            this.trademarkLabel = new System.Windows.Forms.Label();
+            this.cardNumberTextBox = new System.Windows.Forms.TextBox();
+            this.cardNumberLabel = new System.Windows.Forms.Label();
+            this.paymentGroupBox.SuspendLayout();
+            this.orderGroupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // paymentGroupBox
+            // 
+            this.paymentGroupBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(202)))), ((int)(((byte)(68)))));
+            this.paymentGroupBox.Controls.Add(this.cardNumberLabel);
+            this.paymentGroupBox.Controls.Add(this.cardNumberTextBox);
+            this.paymentGroupBox.Controls.Add(this.phoneTextBox);
+            this.paymentGroupBox.Controls.Add(this.phoneLabel);
+            this.paymentGroupBox.Controls.Add(this.serviceLabel);
+            this.paymentGroupBox.Controls.Add(this.addressTextBox);
+            this.paymentGroupBox.Controls.Add(this.pickupRadioButton);
+            this.paymentGroupBox.Controls.Add(this.CVVTextBox);
+            this.paymentGroupBox.Controls.Add(this.deliveryRadioButton);
+            this.paymentGroupBox.Controls.Add(this.CVVLabel);
+            this.paymentGroupBox.Controls.Add(this.addressLabel);
+            this.paymentGroupBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.paymentGroupBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(90)))), ((int)(((byte)(37)))));
+            this.paymentGroupBox.Location = new System.Drawing.Point(63, 11);
+            this.paymentGroupBox.Margin = new System.Windows.Forms.Padding(2);
+            this.paymentGroupBox.Name = "paymentGroupBox";
+            this.paymentGroupBox.Padding = new System.Windows.Forms.Padding(2);
+            this.paymentGroupBox.Size = new System.Drawing.Size(343, 433);
+            this.paymentGroupBox.TabIndex = 4;
+            this.paymentGroupBox.TabStop = false;
+            this.paymentGroupBox.Text = "Payment";
+            // 
+            // phoneTextBox
+            // 
+            this.phoneTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.phoneTextBox.Location = new System.Drawing.Point(14, 259);
+            this.phoneTextBox.Name = "phoneTextBox";
+            this.phoneTextBox.Size = new System.Drawing.Size(238, 20);
+            this.phoneTextBox.TabIndex = 10;
+            // 
+            // phoneLabel
+            // 
+            this.phoneLabel.AutoSize = true;
+            this.phoneLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.phoneLabel.Location = new System.Drawing.Point(12, 241);
+            this.phoneLabel.Name = "phoneLabel";
+            this.phoneLabel.Size = new System.Drawing.Size(103, 15);
+            this.phoneLabel.TabIndex = 9;
+            this.phoneLabel.Text = "Phone Number";
+            // 
+            // serviceLabel
+            // 
+            this.serviceLabel.AutoSize = true;
+            this.serviceLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.serviceLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.serviceLabel.Location = new System.Drawing.Point(11, 316);
+            this.serviceLabel.Name = "serviceLabel";
+            this.serviceLabel.Size = new System.Drawing.Size(100, 15);
+            this.serviceLabel.TabIndex = 8;
+            this.serviceLabel.Text = "Service Option";
+            // 
+            // addressTextBox
+            // 
+            this.addressTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addressTextBox.Location = new System.Drawing.Point(14, 197);
+            this.addressTextBox.Name = "addressTextBox";
+            this.addressTextBox.Size = new System.Drawing.Size(238, 20);
+            this.addressTextBox.TabIndex = 7;
+            // 
+            // pickupRadioButton
+            // 
+            this.pickupRadioButton.AutoSize = true;
+            this.pickupRadioButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pickupRadioButton.ForeColor = System.Drawing.Color.Black;
+            this.pickupRadioButton.Location = new System.Drawing.Point(15, 373);
+            this.pickupRadioButton.Name = "pickupRadioButton";
+            this.pickupRadioButton.Size = new System.Drawing.Size(74, 24);
+            this.pickupRadioButton.TabIndex = 6;
+            this.pickupRadioButton.TabStop = true;
+            this.pickupRadioButton.Text = "Pickup";
+            this.pickupRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // CVVTextBox
+            // 
+            this.CVVTextBox.BackColor = System.Drawing.Color.White;
+            this.CVVTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CVVTextBox.ForeColor = System.Drawing.Color.White;
+            this.CVVTextBox.Location = new System.Drawing.Point(14, 136);
+            this.CVVTextBox.Name = "CVVTextBox";
+            this.CVVTextBox.Size = new System.Drawing.Size(74, 20);
+            this.CVVTextBox.TabIndex = 5;
+            // 
+            // deliveryRadioButton
+            // 
+            this.deliveryRadioButton.AutoSize = true;
+            this.deliveryRadioButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.deliveryRadioButton.ForeColor = System.Drawing.Color.Black;
+            this.deliveryRadioButton.Location = new System.Drawing.Point(15, 343);
+            this.deliveryRadioButton.Name = "deliveryRadioButton";
+            this.deliveryRadioButton.Size = new System.Drawing.Size(82, 24);
+            this.deliveryRadioButton.TabIndex = 4;
+            this.deliveryRadioButton.TabStop = true;
+            this.deliveryRadioButton.Text = "Delivery";
+            this.deliveryRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // CVVLabel
+            // 
+            this.CVVLabel.AutoSize = true;
+            this.CVVLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CVVLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.CVVLabel.Location = new System.Drawing.Point(11, 117);
+            this.CVVLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.CVVLabel.Name = "CVVLabel";
+            this.CVVLabel.Size = new System.Drawing.Size(37, 16);
+            this.CVVLabel.TabIndex = 3;
+            this.CVVLabel.Text = "CVV";
+            // 
+            // addressLabel
+            // 
+            this.addressLabel.AutoSize = true;
+            this.addressLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addressLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.addressLabel.Location = new System.Drawing.Point(11, 178);
+            this.addressLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.addressLabel.Name = "addressLabel";
+            this.addressLabel.Size = new System.Drawing.Size(65, 16);
+            this.addressLabel.TabIndex = 1;
+            this.addressLabel.Text = "Address";
+            // 
+            // orderGroupBox
+            // 
+            this.orderGroupBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(202)))), ((int)(((byte)(68)))));
+            this.orderGroupBox.Controls.Add(this.checkoutButton);
+            this.orderGroupBox.Controls.Add(this.totalLabel);
+            this.orderGroupBox.Controls.Add(this.totalTextBox);
+            this.orderGroupBox.Controls.Add(this.orderListBox);
+            this.orderGroupBox.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.orderGroupBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.orderGroupBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.orderGroupBox.Location = new System.Drawing.Point(448, 11);
+            this.orderGroupBox.Margin = new System.Windows.Forms.Padding(2);
+            this.orderGroupBox.Name = "orderGroupBox";
+            this.orderGroupBox.Padding = new System.Windows.Forms.Padding(2);
+            this.orderGroupBox.Size = new System.Drawing.Size(343, 433);
+            this.orderGroupBox.TabIndex = 5;
+            this.orderGroupBox.TabStop = false;
+            this.orderGroupBox.Text = "Your Order";
+            // 
+            // checkoutButton
+            // 
+            this.checkoutButton.BackColor = System.Drawing.Color.Gold;
+            this.checkoutButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkoutButton.Location = new System.Drawing.Point(182, 362);
+            this.checkoutButton.Name = "checkoutButton";
+            this.checkoutButton.Size = new System.Drawing.Size(144, 39);
+            this.checkoutButton.TabIndex = 3;
+            this.checkoutButton.Text = "Checkout";
+            this.checkoutButton.UseVisualStyleBackColor = false;
+            // 
+            // totalLabel
+            // 
+            this.totalLabel.AutoSize = true;
+            this.totalLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.totalLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.totalLabel.Location = new System.Drawing.Point(10, 371);
+            this.totalLabel.Name = "totalLabel";
+            this.totalLabel.Size = new System.Drawing.Size(43, 16);
+            this.totalLabel.TabIndex = 2;
+            this.totalLabel.Text = "Total";
+            // 
+            // totalTextBox
+            // 
+            this.totalTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.totalTextBox.Location = new System.Drawing.Point(76, 363);
+            this.totalTextBox.Name = "totalTextBox";
+            this.totalTextBox.Size = new System.Drawing.Size(100, 38);
+            this.totalTextBox.TabIndex = 1;
+            // 
+            // orderListBox
+            // 
+            this.orderListBox.BackColor = System.Drawing.Color.White;
+            this.orderListBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.orderListBox.FormattingEnabled = true;
+            this.orderListBox.ItemHeight = 20;
+            this.orderListBox.Location = new System.Drawing.Point(16, 56);
+            this.orderListBox.Name = "orderListBox";
+            this.orderListBox.ScrollAlwaysVisible = true;
+            this.orderListBox.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+            this.orderListBox.Size = new System.Drawing.Size(310, 284);
+            this.orderListBox.TabIndex = 0;
+            // 
+            // trademarkLabel
+            // 
+            this.trademarkLabel.AutoSize = true;
+            this.trademarkLabel.Location = new System.Drawing.Point(362, 467);
+            this.trademarkLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.trademarkLabel.Name = "trademarkLabel";
+            this.trademarkLabel.Size = new System.Drawing.Size(137, 13);
+            this.trademarkLabel.TabIndex = 6;
+            this.trademarkLabel.Text = "©2023 Trevy\'s Iconic Pizza";
+            // 
+            // cardNumberTextBox
+            // 
+            this.cardNumberTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cardNumberTextBox.Location = new System.Drawing.Point(14, 75);
+            this.cardNumberTextBox.Name = "cardNumberTextBox";
+            this.cardNumberTextBox.Size = new System.Drawing.Size(238, 20);
+            this.cardNumberTextBox.TabIndex = 11;
+            // 
+            // cardNumberLabel
+            // 
+            this.cardNumberLabel.AutoSize = true;
+            this.cardNumberLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cardNumberLabel.Location = new System.Drawing.Point(11, 56);
+            this.cardNumberLabel.Name = "cardNumberLabel";
+            this.cardNumberLabel.Size = new System.Drawing.Size(98, 16);
+            this.cardNumberLabel.TabIndex = 12;
+            this.cardNumberLabel.Text = "Card Number";
+            // 
+            // CheckoutPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(202)))), ((int)(((byte)(68)))));
+            this.BackgroundImage = global::TrevysIconicPizza.Properties.Resources.Pizza;
+            this.ClientSize = new System.Drawing.Size(906, 507);
+            this.Controls.Add(this.trademarkLabel);
+            this.Controls.Add(this.orderGroupBox);
+            this.Controls.Add(this.paymentGroupBox);
+            this.Name = "CheckoutPage";
+            this.Text = "Checkout Page";
+            this.paymentGroupBox.ResumeLayout(false);
+            this.paymentGroupBox.PerformLayout();
+            this.orderGroupBox.ResumeLayout(false);
+            this.orderGroupBox.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox paymentGroupBox;
+        private System.Windows.Forms.TextBox phoneTextBox;
+        private System.Windows.Forms.Label phoneLabel;
+        private System.Windows.Forms.Label serviceLabel;
+        private System.Windows.Forms.TextBox addressTextBox;
+        private System.Windows.Forms.RadioButton pickupRadioButton;
+        private System.Windows.Forms.TextBox CVVTextBox;
+        private System.Windows.Forms.RadioButton deliveryRadioButton;
+        private System.Windows.Forms.Label CVVLabel;
+        private System.Windows.Forms.Label addressLabel;
+        private System.Windows.Forms.GroupBox orderGroupBox;
+        private System.Windows.Forms.Button checkoutButton;
+        private System.Windows.Forms.Label totalLabel;
+        private System.Windows.Forms.TextBox totalTextBox;
+        private System.Windows.Forms.ListBox orderListBox;
+        private System.Windows.Forms.Label trademarkLabel;
+        private System.Windows.Forms.Label cardNumberLabel;
+        private System.Windows.Forms.TextBox cardNumberTextBox;
+    }
+}
