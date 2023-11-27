a2367e771c2a511c42c4987ea826284587572ffc
Baelong <fruhamsan@gmail.com>
2023-11-26 20:14:30 -0500

    Verify
    cvv
    card #
    phone #

--- Trevys-Iconic-Pizza--TrevBranch2/Trev_TrevysIconicPizza/Trev_TrevysIconicPizza/TrevysIconicPizza/CheckoutPage.cs
+++ Trevys-Iconic-Pizza--TrevBranch2/Trev_TrevysIconicPizza/Trev_TrevysIconicPizza/TrevysIconicPizza/CheckoutPage.cs
@@ -10,8 +10,10 @@ using System.Windows.Forms;
 
 namespace TrevysIconicPizza
 {
+
     public partial class CheckoutPage : Form
     {
+        List<string> invalidResult = new List<string>();
 
         private CartPage cartPage;
         public CheckoutPage(CartPage cartPageInstance)
@@ -53,6 +55,73 @@ namespace TrevysIconicPizza
             };
 
             cartPage.Show();
+        }
+
+        private bool verifyCard()
+        {
+            bool result = true;
+            if (!System.Text.RegularExpressions.Regex.IsMatch(cardNumberTextBox.Text, @"^(?! )[\d ]{13,19}$"))
+            {
+                invalidResult.Add("Invalid Card input\n");
+                cardNumberTextBox.Clear();
+                result = false;
+            }
+
+            return result;
+        }
+
+        private bool verifyCVV()
+        {
+            bool result = true;
+            if (!System.Text.RegularExpressions.Regex.IsMatch(CVVTextBox.Text, @"^\d{3}$"))
+            {
+                invalidResult.Add("Invalid CVV input\n");
+                CVVTextBox.Clear();
+                result = false;
+            }
+
+            return result;
+        }
+
+        private bool verifyPhone()
+        {
+            bool result = true;
+            if (!System.Text.RegularExpressions.Regex.IsMatch(phoneTextBox.Text, @"^\d{10}$"))
+            {
+                invalidResult.Add("Invalid Phone Number input\n");
+                cardNumberTextBox.Clear();
+                result = false;
+            }
+
+            return result;
+        }
+
+        private void checkoutButton_Click(object sender, EventArgs e)
+        {
+            // Clear the list of invalid results after displaying them
+            invalidResult.Clear();
+
+            string text = "";
+
+            verifyCard();
+            verifyCVV();
+            verifyPhone();
+          
+
+            // If invalidResult is not empty then add all strings to text variable so all errors can be displayed at once
+            if (invalidResult.Count != 0)
+            {
+                foreach (var result in invalidResult)
+                {
+                    text += result;
+                }
+
+                MessageBox.Show(text, "Validation Errors", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                // Clear text after loop
+                text = "";
+
+            }
         }
 
     }
