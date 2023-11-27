55712e4426d56f386cc98d41d543cfd5e82b51c4
Baelong <113056090+Baelong@users.noreply.github.com>
2023-11-27 12:54:28 -0500

    Order class methods
    added get and add methods for pizza/drinks

--- Trevys-Iconic-Pizza--TrevBranch2/Trev_TrevysIconicPizza/Trev_TrevysIconicPizza/TrevysIconicPizza/Order.cs
+++ Trevys-Iconic-Pizza--TrevBranch2/Trev_TrevysIconicPizza/Trev_TrevysIconicPizza/TrevysIconicPizza/Order.cs
@@ -8,10 +8,29 @@ namespace TrevysIconicPizza
 {
     internal class Order
     {
+        private Customer customer { get; set; }
         private List<Pizza> pizzaList;
         private List<Drink> drinkList;
 
+        public List<Drink> GetDrinks()
+        {
+            return new List<Drink>(drinkList);
+        }
 
+        public List<Pizza> GetPizzas()
+        {
+            return new List<Pizza>(pizzaList);
+        }
+
+        public void AddDrink(Drink drink)
+        {
+            drinkList.Add(drink);
+        }
+
+        public void AddPizza(Pizza pizza)
+        {
+            pizzaList.Add(pizza);
+        }
     }
 }
 
