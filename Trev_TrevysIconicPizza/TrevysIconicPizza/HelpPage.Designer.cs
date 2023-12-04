4082a384662759d9d55c841b5891cb9fede26ba3
Baelong <113056090+Baelong@users.noreply.github.com>
2023-12-04 14:59:05 -0500

    help page
    info

--- Trev_TrevysIconicPizza/TrevysIconicPizza/HelpPage.Designer.cs
+++ Trev_TrevysIconicPizza/TrevysIconicPizza/HelpPage.Designer.cs
@@ -30,11 +30,16 @@
         {
             this.helpTabControl = new System.Windows.Forms.TabControl();
             this.accountTabPage = new System.Windows.Forms.TabPage();
+            this.accountManagementLabel = new System.Windows.Forms.Label();
             this.orderTabPage = new System.Windows.Forms.TabPage();
             this.cartTabPage = new System.Windows.Forms.TabPage();
             this.checkoutTabPage = new System.Windows.Forms.TabPage();
             this.helpLabel = new System.Windows.Forms.Label();
+            this.accountCreationLabel = new System.Windows.Forms.Label();
+            this.accountCreationHelpLabel = new System.Windows.Forms.Label();
+            this.accountManagementHelpLabel = new System.Windows.Forms.Label();
             this.helpTabControl.SuspendLayout();
+            this.accountTabPage.SuspendLayout();
             this.SuspendLayout();
             // 
             // helpTabControl
@@ -52,12 +57,27 @@
             // accountTabPage
             // 
             this.accountTabPage.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(202)))), ((int)(((byte)(68)))));
+            this.accountTabPage.Controls.Add(this.accountManagementHelpLabel);
+            this.accountTabPage.Controls.Add(this.accountCreationHelpLabel);
+            this.accountTabPage.Controls.Add(this.accountCreationLabel);
+            this.accountTabPage.Controls.Add(this.accountManagementLabel);
             this.accountTabPage.Location = new System.Drawing.Point(4, 22);
             this.accountTabPage.Name = "accountTabPage";
             this.accountTabPage.Padding = new System.Windows.Forms.Padding(3);
             this.accountTabPage.Size = new System.Drawing.Size(768, 343);
             this.accountTabPage.TabIndex = 0;
             this.accountTabPage.Text = "Account";
+            // 
+            // accountManagementLabel
+            // 
+            this.accountManagementLabel.AutoSize = true;
+            this.accountManagementLabel.Font = new System.Drawing.Font("Arial", 16.2F, System.Drawing.FontStyle.Bold);
+            this.accountManagementLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(65)))), ((int)(((byte)(11)))));
+            this.accountManagementLabel.Location = new System.Drawing.Point(22, 167);
+            this.accountManagementLabel.Name = "accountManagementLabel";
+            this.accountManagementLabel.Size = new System.Drawing.Size(240, 26);
+            this.accountManagementLabel.TabIndex = 0;
+            this.accountManagementLabel.Text = "Account Management:";
             // 
             // orderTabPage
             // 
@@ -101,6 +121,38 @@
             this.helpLabel.Text = "Trevy\'s Iconic Pizza Help";
             this.helpLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
+            // accountCreationLabel
+            // 
+            this.accountCreationLabel.AutoSize = true;
+            this.accountCreationLabel.Font = new System.Drawing.Font("Arial", 16.2F, System.Drawing.FontStyle.Bold);
+            this.accountCreationLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(65)))), ((int)(((byte)(11)))));
+            this.accountCreationLabel.Location = new System.Drawing.Point(22, 25);
+            this.accountCreationLabel.Name = "accountCreationLabel";
+            this.accountCreationLabel.Size = new System.Drawing.Size(196, 26);
+            this.accountCreationLabel.TabIndex = 1;
+            this.accountCreationLabel.Text = "Account Creation:";
+            // 
+            // accountCreationHelpLabel
+            // 
+            this.accountCreationHelpLabel.Font = new System.Drawing.Font("Arial", 11F);
+            this.accountCreationHelpLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(65)))), ((int)(((byte)(11)))));
+            this.accountCreationHelpLabel.Location = new System.Drawing.Point(24, 69);
+            this.accountCreationHelpLabel.Name = "accountCreationHelpLabel";
+            this.accountCreationHelpLabel.Size = new System.Drawing.Size(100, 23);
+            this.accountCreationHelpLabel.TabIndex = 2;
+            this.accountCreationHelpLabel.Text = "label1";
+            // 
+            // accountManagementHelpLabel
+            // 
+            this.accountManagementHelpLabel.AutoSize = true;
+            this.accountManagementHelpLabel.Font = new System.Drawing.Font("Arial", 11F);
+            this.accountManagementHelpLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(65)))), ((int)(((byte)(11)))));
+            this.accountManagementHelpLabel.Location = new System.Drawing.Point(24, 211);
+            this.accountManagementHelpLabel.Name = "accountManagementHelpLabel";
+            this.accountManagementHelpLabel.Size = new System.Drawing.Size(46, 17);
+            this.accountManagementHelpLabel.TabIndex = 3;
+            this.accountManagementHelpLabel.Text = "label1";
+            // 
             // HelpPage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -113,6 +165,8 @@
             this.Name = "HelpPage";
             this.Text = "HelpPage";
             this.helpTabControl.ResumeLayout(false);
+            this.accountTabPage.ResumeLayout(false);
+            this.accountTabPage.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -125,5 +179,9 @@
         private System.Windows.Forms.Label helpLabel;
         private System.Windows.Forms.TabPage cartTabPage;
         private System.Windows.Forms.TabPage checkoutTabPage;
+        private System.Windows.Forms.Label accountManagementLabel;
+        private System.Windows.Forms.Label accountCreationLabel;
+        private System.Windows.Forms.Label accountCreationHelpLabel;
+        private System.Windows.Forms.Label accountManagementHelpLabel;
     }
 }
