8af35a1567b85ea6f23e34017b8285f35876ef4f
Ceveline <74314874+ceveline@users.noreply.github.com>
2023-12-04 14:58:09 -0500

    the marg pizza button is fixed

--- Trev_TrevysIconicPizza/TrevysIconicPizza/PizzaPage.cs
+++ Trev_TrevysIconicPizza/TrevysIconicPizza/PizzaPage.cs
@@ -147,7 +147,7 @@ namespace TrevysIconicPizza
 
         private void margAddToCartButton4_Click(object sender, EventArgs e)
         {
-            
+
             string size = margComboBox.Text;
 
             if (size != "Select Size")
@@ -161,10 +161,10 @@ namespace TrevysIconicPizza
             {
                 MessageBox.Show("Please select a proper size!");
             }
-            
+
         }
 
-        
+
         private void cartLogo_Click(object sender, EventArgs e)
         {
             // Handle the FormClosed event
